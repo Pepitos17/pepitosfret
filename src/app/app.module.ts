3eb71554bc95cea69ba21b805528391e0929764a
Pepitos17 <msogodogo@gmail.com>
2023-09-30 10:10:18 +0200

    firt commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientListComponent } from './client-list/client-list.component';
+import { TransporterListComponent } from './transporter-list/transporter-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientListComponent,
+    TransporterListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/transporter-list/transporter-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transporter-list',
+  templateUrl: './transporter-list.component.html',
+  styleUrls: ['./transporter-list.component.css']
+})
+export class TransporterListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
